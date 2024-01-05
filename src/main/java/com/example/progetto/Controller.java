34a99b13d38494a9d3d8e05bc8298c1fc9573572
lucaaa02 <luca.lomastro@students.uniroma2.eu>
2024-01-05 18:50:24 +0100

    sonar

--- src/main/java/com/example/progetto/Controller.java
+++ src/main/java/com/example/progetto/Controller.java
@@ -3,7 +3,7 @@ package com.example.progetto;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
