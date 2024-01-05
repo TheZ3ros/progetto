9600706f7b796df21fd714d3ae5b5618b4774614
TheZ3ros <alepod9806@gmail.com>
2024-01-05 18:23:51 +0100

    Prova

--- src/main/java/com/example/progetto/HelloApplication.java
+++ src/main/java/com/example/progetto/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-diocane
+Prova
     public static void main(String[] args) {
         launch();
     }
