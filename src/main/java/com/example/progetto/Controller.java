23a2f34a0c032d6ab8649d87fafcee91af0bd629
lucaaa02 <luca.lomastro@students.uniroma2.eu>
2024-01-07 17:08:01 +0100

    home

--- src/main/java/com/example/progetto/Controller.java
+++ src/main/java/com/example/progetto/Controller.java
@@ -1,7 +1,10 @@
 package com.example.progetto;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class Controller {
+
+   public void ricarica(ActionEvent event){}
 }
