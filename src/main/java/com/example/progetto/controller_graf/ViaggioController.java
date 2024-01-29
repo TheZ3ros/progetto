de9dcfd77cc09ff8e63048c42d660653138d8acb
lucaaa02 <luca.lomastro@students.uniroma2.eu>
2024-01-30 00:22:46 +0100

    caricamento viaggio(quasi funziona)

--- src/main/java/com/example/progetto/controller_graf/ViaggioController.java
+++ src/main/java/com/example/progetto/controller_graf/ViaggioController.java
@@ -1,12 +1,20 @@
 package com.example.progetto.controller_graf;
 
+import com.example.progetto.Applicazione;
 import com.example.progetto.bean.TripBean;
+import com.example.progetto.bean.UserBean;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Objects;
 
 public class ViaggioController {
@@ -18,22 +26,44 @@ public class ViaggioController {
     private Text data;
     @FXML
     private Text prezzo;
+    private Applicazione main;
+    private TripBean bean;
+    private UserBean user;
     @FXML
-    private Text posti;
+    public void setMain(Applicazione main){
 
+        this.main = main;
+    }
 
+    public void setUser(UserBean user){
+        this.user=user;
+    }
 
     public void createbox(TripBean bean) {
-
+        this.bean=bean;
         imagine.setImage(new Image(Objects.requireNonNull(getClass().getResource(bean.image())).toExternalForm()));
             title.setText(bean.getCity());
             data.setText(bean.getData_and() +"/" + bean.getData_rit());
             prezzo.setText((int)bean.getPrice()+"€");
-        //    posti.setText("posti disponibili:" + bean.getPlaces());
-        //  prenota.setText("prenota");
         }
-    public void pagetrip(){
-        System.out.println("cliccato");
+    public void pagetrip() {
+        try {
+            FXMLLoader paginaLoader = new FXMLLoader(Applicazione.class.getResource("pagetrip.fxml"));
+            Parent PageRoot = paginaLoader.load();
+            Scene paginaScene = new Scene(PageRoot);
+            PageTripController pagetrip = paginaLoader.getController();
+            pagetrip.setMain(main);
+            pagetrip.set_trip(bean);
+            pagetrip.setUserBean(user);
+            Stage stage = main.getStage();
+            stage.setScene(paginaScene);
+            pagetrip.charge();
+            pagetrip.setButtonText();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     }
