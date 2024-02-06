4cf7e6bc56af8460148091e5d9063e80e331f105
TheZ3ros <alepod9806@gmail.com>
2024-02-06 19:56:48 +0100

    fix code smell

--- src/main/java/com/example/progetto/dao/AgencyDAO.java
+++ src/main/java/com/example/progetto/dao/AgencyDAO.java
@@ -15,27 +15,42 @@ public class AgencyDAO implements GenericDAO <Agency> {
     @Override
     public Agency execute(Object... params) throws SQLException {
         String username = (String) params[0];
-        Agency utente=new Agency();
-        try{
+        Agency utente = new Agency();
+        try {
             connection.connected();
             CallableStatement cs = connection.conn.prepareCall("{call GetPasswordAgenzia(?,?)}");
-            cs.setString(1,username);
-            cs.registerOutParameter(2, Types.VARCHAR);
-            cs.executeQuery();
-            utente.setPassword(cs.getString(2));
-            utente.setUser((String) params[0]);
+            try {
+                cs.setString(1, username);
+                cs.registerOutParameter(2, Types.VARCHAR);
+                cs.executeQuery();
+                utente.setPassword(cs.getString(2));
+                utente.setUser((String) params[0]);
+            } catch (SQLException queryException) {
+                // Gestisci l'eccezione se si verifica un errore durante l'esecuzione della query
+                queryException.printStackTrace();
+            } finally {
+                // Chiudi lo statement
+                if (cs != null) {
+                    try {
+                        cs.close();
+                    } catch (SQLException e) {
+                        // Gestisci l'eccezione se si verifica un errore durante la chiusura dello statement
+                        e.printStackTrace();
+                    }
+                }
+            }
         } finally {
+            // Chiudi la connessione
             if (connection != null && connection.conn != null) {
                 try {
                     connection.conn.close();
                 } catch (SQLException e) {
+                    // Gestisci l'eccezione se si verifica un errore durante la chiusura della connessione
                     e.printStackTrace();
                 }
             }
         }
-
-
-
         return utente;
     }
+
 }
