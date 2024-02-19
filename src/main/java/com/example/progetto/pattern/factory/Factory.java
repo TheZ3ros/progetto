5afc7e3fe7a85af2d8b6e84a87a834cf61324e24
TheZ3ros <alepod9806@gmail.com>
2024-02-19 19:20:14 +0100

    code smell

--- src/main/java/com/example/progetto/pattern/factory/Factory.java
+++ src/main/java/com/example/progetto/pattern/factory/Factory.java
@@ -4,11 +4,11 @@ import com.example.progetto.bean.AgencyBean;
 import com.example.progetto.bean.UserBean;
 
 public class Factory{
-    public BeanFactory createBean(int type) throws IllegalArgumentException{
+    public BeanFactory createBean(int type) throws Exception{
         return switch (type) {
             case 1 -> new UserBean();
             case 2 -> new AgencyBean();
-            default -> throw new IllegalArgumentException("Invalid type : " + type);
+            default -> throw new Exception("Invalid type : " + type);
         };
     }
 
