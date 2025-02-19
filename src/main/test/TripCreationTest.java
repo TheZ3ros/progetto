946059c477776d829631834c5297b46e39b7daef
TheZ3ros <alepod9806@gmail.com>
2025-02-19 14:46:14 +0100

    code smell

--- src/main/test/TripCreationTest.java
+++ src/main/test/TripCreationTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TripCreationTest {
     /*
-    Authore of the test: Podda Alessandro
+    Author of the test: Podda Alessandro
     MATRICOLA: 0316851
 
      */
